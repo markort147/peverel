1af77b90ef00395f9e1e2a400fca923c3a6d94ad
markort147 <markort147@gmail.com>
2025-04-13 00:21:51 +0100

    fixed docker compose

--- cmd/notifier/main.go
+++ cmd/notifier/main.go
@@ -56,15 +56,18 @@ func main() {
 	// init template
 	tmpl, err := template.New("templates").ParseFS(assetsFS, "assets/templates/*")
 	if err != nil {
-		panic(err)
+		log.Logger.Errorf("Error parsing templates: %v", err)
+		os.Exit(1)
 	}
 
 	// set up the scheduler
 	if scheduledTime != "" {
 		// parse the scheduled time
-		parsedTime, _ := time.Parse("15:04", scheduledTime)
+		parsedTime, _ := time.Parse("15:04-07", scheduledTime)
 		now := time.Now()
 		schedule := time.Date(now.Year(), now.Month(), now.Day(), parsedTime.Hour(), parsedTime.Minute(), 0, 0, now.Location())
+
+		log.Logger.Infof("Service started with scheduled time: %s. Now is %s.", schedule.Format("15:04"), now.Format("15:04"))
 
 		// if the scheduled time is today, add 24 hours to it
 		initialDuration := schedule.Sub(now)
@@ -72,14 +75,17 @@ func main() {
 			initialDuration = time.Duration(scheduledHours) * time.Hour
 		}
 		// send the email after the initial duration
+		log.Logger.Infof("Waiting for next tick: %f mins", initialDuration.Minutes())
 		time.Sleep(initialDuration)
 		sendEmail(tmpl, data, emailSender, emailRecipients, smtpServer, smtpPort, smtpUsername, smtpPassword)
+		log.Logger.Infof("Waiting for next tick")
 
 		// set up a ticker to send the email every 24 hours
 		ticker := time.NewTicker(24 * time.Hour)
 		defer ticker.Stop()
 		for range ticker.C {
 			sendEmail(tmpl, data, emailSender, emailRecipients, smtpServer, smtpPort, smtpUsername, smtpPassword)
+			log.Logger.Infof("Waiting for next tick")
 		}
 	} else {
 		// if the scheduled time is not set, send the email immediately
@@ -91,11 +97,13 @@ func sendEmail(tmpl *template.Template, data *ts.PsqlData, emailSender string, e
 	// get the expired tasks
 	expiredTasks, err := data.Tasks("", "0", true)
 	if err != nil {
-		panic(err)
+		log.Logger.Errorf("Error getting expired tasks: %v", err)
+		return
 	}
 
 	if len(expiredTasks) == 0 {
 		// no expired tasks, do not send an email
+		log.Logger.Infof("No expired tasks found")
 		return
 	} else {
 		// build the tasks list
@@ -115,7 +123,8 @@ func sendEmail(tmpl *template.Template, data *ts.PsqlData, emailSender string, e
 			"Tasks": tasks,
 			"Count": len(expiredTasks),
 		}); err != nil {
-			panic(err)
+			log.Logger.Errorf("Error executing template: %v", err)
+			return
 		}
 
 		// set up the email message
@@ -128,10 +137,9 @@ func sendEmail(tmpl *template.Template, data *ts.PsqlData, emailSender string, e
 		// send the email
 		dialer := gomail.NewDialer(smtpServer, smtpPort, smtpUsername, smtpPassword)
 		if err := dialer.DialAndSend(message); err != nil {
-			fmt.Println("Error:", err)
-			panic(err)
+			log.Logger.Errorf("Error sending email: %v", err)
 		} else {
-			fmt.Println("Email sent successfully!")
+			log.Logger.Infof("Email sent succesfully")
 		}
 	}
 }
