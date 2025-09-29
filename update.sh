7bd59cec0e8a846975100772c710f5b93b4690de
Marco <markort147@gmail.com>
2025-09-29 23:27:18 +0100

    update.sh is executable

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 go build -o ./build/peverel ./cmd/peverel/
 go build -o ./build/peverel-notifier ./cmd/notifier/
-sudo install -o root -g root -m 0755 ./peverel /usr/local/bin/peverel
-sudo install -o root -g root -m 0755 ./peverel-notifier /usr/local/bin/peverel-notifier
+sudo install -o root -g root -m 0755 ./build/peverel /usr/local/bin/peverel
+sudo install -o root -g root -m 0755 ./build/peverel-notifier /usr/local/bin/peverel-notifier
